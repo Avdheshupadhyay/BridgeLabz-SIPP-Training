9dd2ffb94eb4642b74c9df60b2a1bde8aabaf1d4
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-06-16 12:02:22 +0530

    Added: Program to Calculate the percentages and grades of students and print

--- Arrays/GradeMarks.java
+++ Arrays/GradeMarks.java
@@ -1,77 +1,70 @@
 // import the package
 import java.util.*;
-public class GradeMarks {
-   public static void main(String [] args){
-       Scanner sc=new Scanner(System.in);
-       // take the input for number of students
-       System.out.println("enter the number of students :");
-       int number=sc.nextInt();
-       // Create a 2D array to store the maths physics and chemistry marks of students
-       int [][]result=new int[number][3];
-       for(int i=0;i<number;i++){
-           for(int j=0;j<3;j++){
-               if(j==0){
-                   System.out.println("enter the Maths Marks of Student "+(i+1)+" =");
-               }
-               else if(j==1){
-                    System.out.println("enter the Physics Marks of Student "+(i+1)+" =");
-               }
-               else{
-                      System.out.println("enter the chemistry Marks of Student "+(i+1)+" =");
-               }
-               int n=sc.nextInt();
-               result[i][j]=n;
-           }
-       }
-       // initilize the array to store  marks , percentage , grade of students
-       int []percentage=new int[number];
-       char []grade=new char[number];
-       for(int i=0;i<number;i++){
-           int sum=0;
-           for(int j=0;j<3;j++){
-               int n=result[i][j];
-               sum+=n;
-           }
-           // check for grade and percentages
-           int percent = (int)(((double) sum / 300) * 100);
-           percentage[i]=percent;
-           if(percent>=80){
-               grade[i]='A';
-           }
-           else if(percent>=70){
-               grade[i]='B';
-           }
-           else if(percent>=60){
-               grade[i]='C';
-           }
-            else if(percent>=50){
-               grade[i]='D';
-           }
-           else if(percent>=40){
-               grade[i]='E';
-           }
-           else{
-               grade[i]='R';
-           }
-           
-       }
-       for(int i=0;i<number;i++){
-           System.out.println((i+1)+"Student Result :");
-           for(int j=0;j<3;j++){
-               if(j==0){
-               System.out.print("Maths Marks = "+result[i][j]+"  ");
-               }
-               else if(j==1){
-               System.out.print("physics Marks = "+result[i][j]+"  ");
-               }
-               else if(j==2){
-               System.out.print("Chemistry Marks = "+result[i][j]+"  ");
-               }
-           }
-           System.out.println();
-           System.out.print("percentage is "+ percentage[i]+"  ");
-           System.out.println("grade is "+ grade[i]);
-       }
-       
-   }
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take input for number of students
+        System.out.println("Enter the number of students:");
+        int number = sc.nextInt();
+
+        // Create a 2D array to store Maths, Physics, Chemistry marks of students
+        int[][] result = new int[number][3];
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < 3; j++) {
+                String subject = "";
+                if (j == 0) subject = "Maths";
+                else if (j == 1) subject = "Physics";
+                else subject = "Chemistry";
+
+                while (true) {
+                    System.out.println("Enter the " + subject + " Marks of Student " + (i + 1) + ":");
+                    int n = sc.nextInt();
+
+                    if (n < 0) {
+                        System.out.println("Invalid input! Marks can't be negative. Please enter again.");
+                        continue; // ask again for same subject
+                    }
+
+                    result[i][j] = n;
+                    break;
+                }
+            }
+        }
+
+        // Initialize arrays to store percentage and grade of each student
+        int[] percentage = new int[number];
+        char[] grade = new char[number];
+
+        for (int i = 0; i < number; i++) {
+            int sum = 0;
+            for (int j = 0; j < 3; j++) {
+                sum += result[i][j];
+            }
+
+            int percent = (int) (((double) sum / 300) * 100);
+            percentage[i] = percent;
+
+            // Assign grade based on percentage
+            if (percent >= 80) grade[i] = 'A';
+            else if (percent >= 70) grade[i] = 'B';
+            else if (percent >= 60) grade[i] = 'C';
+            else if (percent >= 50) grade[i] = 'D';
+            else if (percent >= 40) grade[i] = 'E';
+            else grade[i] = 'R';
+        }
+
+        // Display each student's results
+        for (int i = 0; i < number; i++) {
+            System.out.println("\nStudent " + (i + 1) + " Result:");
+            System.out.println("Maths Marks     = " + result[i][0]);
+            System.out.println("Physics Marks   = " + result[i][1]);
+            System.out.println("Chemistry Marks = " + result[i][2]);
+            System.out.println("Percentage      = " + percentage[i] + "%");
+            System.out.println("Grade           = " + grade[i]);
+        }
+
+        sc.close();
+    }
 }
