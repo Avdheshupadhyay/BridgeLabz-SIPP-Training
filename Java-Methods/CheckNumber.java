1b29645df43e06bf7a60d79fd38adf2259c6d42d
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-06-17 11:18:07 +0530

    Refactor: Added the Comments in previous code

--- Java-Methods/CheckNumber.java
+++ Java-Methods/CheckNumber.java
@@ -4,14 +4,18 @@ public class CheckNumber {
         if(number>0){
             return 1;
         }
+        // apply the condition
         return number<0?-1:0;
     }
       public static void main(String []args){
         Scanner sc=new Scanner(System.in);
+        // take input from user
         System.out.println("enter the number :");
         int number=sc.nextInt();
         sc.close();
+        // create a object
         CheckNumber checknumber=new CheckNumber();
+        // call the method
         System.out.println(checknumber.solve(number));
         
       }
