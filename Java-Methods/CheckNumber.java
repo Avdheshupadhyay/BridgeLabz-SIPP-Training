deafd112152517b4ac20f6124a51ad3d912b254f
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-06-17 09:48:56 +0530

    Added: Write a method to Check a number positive negative or zero

---
+++ Java-Methods/CheckNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class CheckNumber {
+    public int solve(int number){
+        if(number>0){
+            return 1;
+        }
+        return number<0?-1:0;
+    }
+      public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number :");
+        int number=sc.nextInt();
+        sc.close();
+        CheckNumber checknumber=new CheckNumber();
+        System.out.println(checknumber.solve(number));
+        
+      }
+}
