d2ccd71170423a7bf0d0f1709e652378a7874a7e
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-06-25 11:28:43 +0530

    Added: CarRental class using constructor

---
+++ Constructor/CarRental.java
@@ -0,0 +1,21 @@
+// CarRental.java
+// Class with customerName, carModel, rentalDays; constructors
+public class CarRental {
+    private String customerName;
+    private String carModel;
+    private int rentalDays;
+
+    public CarRental() {
+        this("Unknown", "Unknown", 0);
+    }
+
+    public CarRental(String customerName, String carModel, int rentalDays) {
+        this.customerName = customerName;
+        this.carModel = carModel;
+        this.rentalDays = rentalDays;
+    }
+
+    public void display() {
+        System.out.println("Customer: " + customerName + ", Car Model: " + carModel + ", Rental Days: " + rentalDays);
+    }
+}
