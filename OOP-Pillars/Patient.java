345b0f04010037c4b0c2e29bb0fbdeda9803ea83
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-07-03 10:55:52 +0530

    Added: Patient superclass showing abstraction and inheritance

---
+++ OOP-Pillars/Patient.java
@@ -0,0 +1,38 @@
+public abstract class Patient {
+    private final int patientId;
+    private String name;
+    private int age;
+
+    public Patient(int patientId, String name, int age) {
+        this.patientId = patientId;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) throw new IllegalArgumentException("Age must be positive");
+        this.age = age;
+    }
+
+    public abstract double calculateBill();
+
+    public void getPatientDetails() {
+        System.out.println("ID: " + patientId + ", Name: " + name + ", Age: " + age);
+    }
+}
