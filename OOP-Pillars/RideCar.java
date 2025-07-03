a3da655529c216abfbb8ce8f13d7afdddfe278b5
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-07-03 10:55:53 +0530

    Added: RideCar class showing runtime polymorphism

---
+++ OOP-Pillars/RideCar.java
@@ -0,0 +1,23 @@
+public class RideCar extends RideVehicle implements GPS {
+    private String location;
+
+    public RideCar(String vehicleId, String driverName, double ratePerKm, String location) {
+        super(vehicleId, driverName, ratePerKm);
+        this.location = location;
+    }
+
+    @Override
+    public double calculateFare(double distance) {
+        return getRatePerKm() * distance;
+    }
+
+    @Override
+    public String getCurrentLocation() {
+        return location;
+    }
+
+    @Override
+    public void updateLocation(String newLocation) {
+        this.location = newLocation;
+    }
+}
