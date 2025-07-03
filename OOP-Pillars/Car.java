4fd46753cc3865eb24240ade6cd528645e086bad
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-07-03 10:55:47 +0530

    Added: Car class extending Vehicle demonstrating inheritance

---
+++ OOP-Pillars/Car.java
@@ -0,0 +1,32 @@
+public class Car extends Vehicle implements Insurable {
+    private String insurancePolicyNumber;
+
+    public Car(String vehicleNumber, double rentalRate, String insurancePolicyNumber) {
+        super(vehicleNumber, "Car", rentalRate);
+        this.insurancePolicyNumber = insurancePolicyNumber;
+    }
+
+    @Override
+    public double calculateRentalCost(int days) {
+        return getRentalRate() * days;
+    }
+
+    @Override
+    public double calculateInsurance() {
+        // Flat insurance for car
+        return 2000;
+    }
+
+    @Override
+    public String getInsuranceDetails() {
+        return "Car Insurance Policy: " + insurancePolicyNumber;
+    }
+
+    public String getInsurancePolicyNumber() {
+        return insurancePolicyNumber;
+    }
+
+    public void setInsurancePolicyNumber(String insurancePolicyNumber) {
+        this.insurancePolicyNumber = insurancePolicyNumber;
+    }
+}
