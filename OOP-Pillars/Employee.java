b1ed5e5201e8c2f00af48a1111ff85d19b9e34f7
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-07-03 10:55:49 +0530

    Added: Employee superclass for inheritance and abstraction

---
+++ OOP-Pillars/Employee.java
@@ -0,0 +1,40 @@
+public abstract class Employee {
+    private final int employeeId;
+    private String name;
+    private double baseSalary;
+
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        setBaseSalary(baseSalary);
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary < 0) {
+            throw new IllegalArgumentException("Salary must be positive");
+        }
+        this.baseSalary = baseSalary;
+    }
+
+    public abstract double calculateSalary();
+
+    public void displayDetails() {
+        System.out.println("ID: " + employeeId + ", Name: " + name + ", Base Salary: " + baseSalary);
+    }
+}
