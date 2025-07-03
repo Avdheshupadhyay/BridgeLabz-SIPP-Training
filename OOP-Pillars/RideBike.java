56e9de9ed3f94a66d8b4efd7018d099409bf7a9c
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-07-03 10:55:53 +0530

    Added: RideBike implementing RideVehicle using polymorphism

---
+++ OOP-Pillars/RideBike.java
@@ -0,0 +1,23 @@
+public class RideBike extends RideVehicle implements GPS {
+    private String location;
+
+    public RideBike(String vehicleId, String driverName, double ratePerKm, String location) {
+        super(vehicleId, driverName, ratePerKm);
+        this.location = location;
+    }
+
+    @Override
+    public double calculateFare(double distance) {
+        return getRatePerKm() * distance * 0.8; // 20% cheaper for bikes
+    }
+
+    @Override
+    public String getCurrentLocation() {
+        return location;
+    }
+
+    @Override
+    public void updateLocation(String newLocation) {
+        this.location = newLocation;
+    }
+}
