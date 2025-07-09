95f71d92be8304d14f8ff1c1fed64ec911e0118a
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-07-09 09:34:37 +0530

    Added: ZeroSumSubarrays using HashMap to detect subarrays with 0 sum

---
+++ HashMap/ZeroSumSubarrays.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class ZeroSumSubarrays {
+    // Finds all subarrays with zero sum
+    public static List<int[]> findZeroSumSubarrays(int[] arr) {
+        List<int[]> result = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int sum = 0;
+        map.put(0, new ArrayList<>(Arrays.asList(-1)));
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (map.containsKey(sum)) {
+                for (int start : map.get(sum)) {
+                    result.add(new int[]{start + 1, i});
+                }
+            }
+            map.computeIfAbsent(sum, k -> new ArrayList<>()).add(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -7, 3, 1, 3, 1, -4, -2, -2};
+        List<int[]> subarrays = findZeroSumSubarrays(arr);
+        System.out.println("Zero sum subarrays (start, end indices):");
+        for (int[] sub : subarrays) {
+            System.out.println(Arrays.toString(sub));
+        }
+    }
+}
