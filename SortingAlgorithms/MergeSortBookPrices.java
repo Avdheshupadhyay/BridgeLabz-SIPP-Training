f99320970f231832cddfc5c599d004a430a09fea
avdheshupadhyay <avdheshupadhyay34@gmail.com>
2025-07-14 08:46:26 +0530

    Added: MergeSortBookPrices sorting book prices using merge sort

---
+++ SortingAlgorithms/MergeSortBookPrices.java
@@ -0,0 +1,54 @@
+// Merge Sort - Sort an Array of Book Prices
+public class MergeSortBookPrices {
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {299, 150, 499, 200, 350};
+        System.out.print("Original prices: ");
+        for (int price : prices) System.out.print(price + " ");
+        System.out.println();
+        mergeSort(prices, 0, prices.length - 1);
+        System.out.print("Sorted prices: ");
+        for (int price : prices) System.out.print(price + " ");
+        System.out.println();
+    }
+}
